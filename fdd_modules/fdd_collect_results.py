9666f882e50bdf953dac72a8117914905be06a0b
Evgeny Zavarygin <z5093467@Evgeny-Z.lan>
2017-03-04 11:57:53 +1100

    Number of iterations are now plotted

--- fdd_modules/fdd_collect_results.py
+++ fdd_modules/fdd_collect_results.py
@@ -10,35 +10,49 @@ class collect_results:
     def __init__(self,result_file_list,abs_path_list,fort_path,ion1,ion2):
         with open(fort_path,'r') as fort:
             len_fort = len(fort.read().split('\n')) # number of lines in original f26
-
         for result_file_name in result_file_list: # for each fd*_resuts.dat file (i.e. each parameter)
             abs_path_sublist = [abs_path for abs_path in abs_path_list if abs_path.split('/')[-2]==result_file_name.split('_')[0]] # sublist for each fdd parameter
             data_list = [] # Put all the results in the list to sort afterwards
             for path in abs_path_sublist:
                 path_to_newf26 = glob(path + '/f26.?????') # look for f26 files
-                if len(path_to_newf26) == 1:
-                    error = 0
-                elif len(path_to_newf26) == 0:
+                # Series of checks for errors =====================================================
+                if len(path_to_newf26) == 0:
                     error = 1 # no f26 was found
-                    data_list.append([float(path.split('/')[-1].split('_')[-1]),error, np.nan,np.nan,np.nan,np.nan])
+                    data_list.append([float(path.split('_')[-1]),error,np.nan,np.nan,np.nan,np.nan,np.nan])
+                    continue
+       	      	elif len(path_to_newf26) > 1:
+		    print " More than one f26 file was found in one folder:\n {}".format(path_to_newf26)
+                    quit()
+                    # continue
+                elif os.path.getsize(path + '/stderr.dat') != 0:
+                    error = 2 # Error happened, check stderr.dat file
+                    data_list.append([float(path.split('_')[-1]),error,np.nan,np.nan,np.nan,np.nan,np.nan])
+                    continue
+                elif os.path.getsize(path_to_newf26[0]) == 0:
+                    error = 3 # zero size f26 file, check stdout.dat
+                    data_list.append([float(path.split('_')[-1]),error,np.nan,np.nan,np.nan,np.nan,np.nan])
                     continue
                 else:
-                    print " More than one f26 file was found in one folder:\n {}".format(path_to_newf26)
-                    quit()
-                # error = 0 => keep going
+                    pass # no errors so far => keep going
                 with open(path_to_newf26[0],'r') as new_f26:
                     len_new_fort = len(new_f26.read().split('\n')) # number of lines in new f26
                 if len_new_fort != len_fort:
-                    error = 2 # new and original f26's have different number of lines
-                    data_list.append([float(path.split('_')[-1]),error,np.nan,np.nan,np.nan,np.nan])
-
-                else: # error = 0 still
+                    error = 4 # new and original f26's have different number of lines
+                    data_list.append([float(path.split('_')[-1]),error,np.nan,np.nan,np.nan,np.nan,np.nan])
+                else:
                     results = self.fort_parse(path_to_newf26[0],ion1,ion2) # array with results
-                    if results[1] == -3 or results[3] == -3:
-                        error = 3 # '*******' uncertainty in f26
-                        data_list.append([float(path.split('_')[-1]),error,np.nan,np.nan,np.nan,np.nan])
+                    if results[1] == -5 or results[3] == -5:
+                        error = 5 # '*******' uncertainty in f26
+                        data_list.append([float(path.split('_')[-1]),error,np.nan,np.nan,np.nan,np.nan,np.nan])
                     else:
-                        data_list.append([float(path.split('_')[-1]),error,results[0],results[1],
+                        with open(path + '/stdout.dat','r') as iter_file:
+			    n_iter = iter_file.read().count('iteration') # count occurrence of 'iteration' in vpfit output
+                            n_iter = n_iter - 1 # remove zero iteration
+                        if n_iter == 125:
+                            error = -1 # indicate that we are not in the minimum yet, more iterations needed
+                        else:
+                            error = 0 # no errors
+                        data_list.append([float(path.split('_')[-1]),error,n_iter,results[0],results[1],
                                           results[2],results[3]])
           
             data_list = np.array(data_list) # Convert to np.array for the following sorting
@@ -48,7 +62,7 @@ class collect_results:
         
     def fort_parse(self,path_to_f26,ion1,ion2):
         """
-        Module is to parse f26 file and get H and D values with uncert.
+        Module is to parse f26 file and get ion1 and ion2 values with uncertainties.
 
         """
         with open(path_to_f26,'r') as new_f26:
@@ -56,27 +70,27 @@ class collect_results:
         if all_lines[-1] =='': # delete last empty line if present
             del all_lines[-1]
         all_ions = [line for line in all_lines if line.split()[0] != "%%" and line.split()[0] != "!"]
-        total_H_line = all_ions[ion1-1].split() # Pick up ions specified in command line
-        total_D_line = all_ions[ion2-1].split()
-        Hcol = total_H_line[6] # 6 for ions with space in lable (H I)
-        Dcol = total_D_line[6] # 5 for those without space (HI)
-        if "*" in total_H_line[7]:
-            Hcol_unc = -3 # will write error
+        total_ion1_line = all_ions[ion1-1].split() # Pick up ions specified in command line
+        total_ion2_line = all_ions[ion2-1].split()
+        ion1_col = total_ion1_line[6] # 6 for ions with space in lable (H I)
+        ion2_col = total_ion2_line[6] # 5 for those without space (HI)
+        if "*" in total_ion1_line[7]:
+            ion1_col_unc = -5 # will write error
         else:
-            Hcol_unc = float(total_H_line[7]) # 7 for ions with space in lable (H I), 6 for those without space (HI)
-        if "*" in total_D_line[7]:
-            Dcol_unc = -3 # will write error
+            ion1_col_unc = float(total_ion1_line[7]) # 7 for ions with space in lable (H I), 6 for those without space (HI)
+        if "*" in total_ion2_line[7]:
+            ion2_col_unc = -5 # will write error
         else:
-            Dcol_unc = float(total_D_line[7]) # 7 for ions with space in lable (D I), 6 for those without space (DI)
-        if Hcol[-1] in ('x','%','X'): # change to whatever you like
-            Hcol = float(Hcol[:-1])   # consider 2 last figures if you used 2 instead of 1
+            ion2_col_unc = float(total_ion2_line[7]) # 7 for ions with space in lable (D I), 6 for those without space (DI)
+        if ion1_col[-1] in ('x','%','X'): # change to whatever you like
+            ion1_col = float(ion1_col[:-1])   # consider 2 last figures if you used 2 instead of 1
         else:
-            Hcol = float(Hcol)
-        if Dcol[-1] in ('x','%','X'):
-            Dcol = float(Dcol[:-1])
+            ion1_col = float(ion1_col)
+        if ion2_col[-1] in ('x','%','X'):
+            ion2_col = float(ion2_col[:-1])
         else:
-            Dcol = float(Dcol)
-        return [Hcol,Hcol_unc,Dcol,Dcol_unc]
+            ion2_col = float(ion2_col)
+        return [ion1_col,ion1_col_unc,ion2_col,ion2_col_unc]
 
 def get_default_fdds(vp_setup_path):
     """
--- fdd_modules/fdd_plot.py
+++ fdd_modules/fdd_plot.py
@@ -7,8 +7,8 @@ import numpy as np
 #------------------------------------------------
 rc('lines',linewidth=1.0)
 rc('font', family='Comic Sans MS')
-rc('font', size=12)
-rc('axes', labelsize=12)
+rc('font', size=10)
+rc('axes', labelsize=10)
 rc('xtick', labelsize=10)
 rc('ytick', labelsize=10)
 
@@ -22,8 +22,8 @@ def fdd_plot(result_file_list,default_fdds):
     # Getting data from the file
     fdd_param_keys = [result_file.split('_')[0] for result_file in result_file_list]
     print "  Parameters to plot for:", fdd_param_keys
-    fig = plt.figure(figsize=(8.27,11.69))
-    axs = [None]*(len(result_file_list)*4+1) # List of axes: 2 ions x (1 uncert + 1 value)
+    fig = plt.figure(figsize=(14,12))#(8.27,11.69))
+    axs = [None]*(len(result_file_list)*5 + 1) # List of axes: 2 ions x (1 uncert + 1 value) + 1 for iterations (axs[0] will not be used)
     for j,result_file_name in enumerate(result_file_list):
         with open(result_file_name,'r') as res_file:
             res_data = res_file.read().split('\n')
@@ -31,69 +31,104 @@ def fdd_plot(result_file_list,default_fdds):
             del res_data[-1]
         else:
             pass
-        fdd_list, H_list, Hunc_list, D_list, Dunc_list = [[] for i in range(5)]
+        fdd_list, H_list, Hunc_list, D_list, Dunc_list, n_iter_list,fdd_iter_list = [[] for i in range(7)]
         for line in res_data:
             line_split = line.split()
-            if line_split[1] == '0':
+            if line_split[1] == '0' or line_split[1] == '-1':
                 fdd_list.append(float(line_split[0].split('_')[-1])) # fdd_list
-                H_list.append(float(line_split[2])) # H_list
-                Hunc_list.append(float(line_split[3])) # Hunc_list
-                D_list.append(float(line_split[4])) # D_list
-                Dunc_list.append(float(line_split[5])) # Dunc_list
+                fdd_iter_list.append(float(line_split[0].split('_')[-1])) # fdd_list for iteration subplot
+                if line_split[1] == '0':
+                    n_iter_list.append(int(line_split[2])) # number of iterations list
+                else:
+                    n_iter_list.append(-1) # 125, number of iterations
+                H_list.append(float(line_split[3])) # H_list
+                Hunc_list.append(float(line_split[4])) # Hunc_list
+                D_list.append(float(line_split[5])) # D_list
+                Dunc_list.append(float(line_split[6])) # Dunc_list
             elif line_split[1] == '1':
                 print "  {} = {} <- excluded: error '1' (no f26 file was found, check 'stdout.dat' and 'stderr.dat')".format(fdd_param_keys[j], line_split[0])
+                fdd_iter_list.append(float(line_split[0].split('_')[-1])) # fdd_list for iteration subplot
+                n_iter_list.append(0) # number of iterations list
             elif line_split[1] == '2':
-                print "  {} = {} <- excluded: error '2' (new and original f26's have different number of lines, check 'stdout.dat')".format(fdd_param_keys[j], line_split[0])
+                print "  {} = {} <- excluded: error '2' (error happened during VPFITing, check 'stderr.dat')".format(fdd_param_keys[j], line_split[0])
+		fdd_iter_list.append(float(line_split[0].split('_')[-1])) # fdd_list for iteration subplot
+		n_iter_list.append(0) # number of iterations list
             elif line_split[1] == '3':
-                print "  {} = {} <- excluded: error '3' ('****' uncertainty was found for one of specified ion! Check f26 file)".format(fdd_param_keys[j], line_split[0])
+                print "  {} = {} <- excluded: error '3' (zero size f26 file, check 'stdout.dat')".format(fdd_param_keys[j], line_split[0])
+		fdd_iter_list.append(float(line_split[0].split('_')[-1])) # fdd_list for iteration subplot
+		n_iter_list.append(0) # number of iterations list
+            elif line_split[1] == '4':
+                print "  {} = {} <- excluded: error '4' (new and original f26's have different number of lines, check 'stdout.dat')".format(fdd_param_keys[j], line_split[0])
+                fdd_iter_list.append(float(line_split[0].split('_')[-1])) # fdd_list for iteration subplot
+		n_iter_list.append(-3) # number of iterations list
+            elif line_split[1] == '5':
+                print "  {} = {} <- excluded: error '5' ('****' uncertainty was found for one of specified ion! Check f26 file)".format(fdd_param_keys[j], line_split[0])
+                fdd_iter_list.append(float(line_split[0].split('_')[-1])) # fdd_list for iteration subplot
+		n_iter_list.append(-2) # number of iterations list
             else:
                 print "  Unknown error code was specified in {}".format(result_file_name)
 
         # Data are ready to plot
         # First column of subplots (uncertainties)
-        axs[4*j+1] = plt.subplot(len(fdd_param_keys),2,2*j+1)
-        axs[4*j+1].plot(fdd_list,Dunc_list,color='b') # Plotting data
-        axs[4*j+1].set_ylabel(r'$\Delta$N(D I)',color='b')
-        axs[4*j+1].tick_params('y',colors='b')
-        axs[4*j+1].ticklabel_format(useOffset=False)
-        axs[4*j+1].set_xscale('log')
+        axs[5*j+1] = plt.subplot(len(fdd_param_keys),3,3*j+1)
+        axs[5*j+1].plot(fdd_list,Dunc_list,color='b') # Plotting data
+        axs[5*j+1].set_ylabel(r'$\Delta$N(D I)',color='b')
+        axs[5*j+1].tick_params('y',colors='b')
+        axs[5*j+1].ticklabel_format(useOffset=False)
+        axs[5*j+1].set_xscale('log')
         if fdd_param_keys[j] == 'fdbstep':
-            axs[4*j+1].set_xlabel('{} [{:g}], km/s'.format(fdd_param_keys[j],default_fdds[fdd_param_keys[j]]))
+            axs[5*j+1].set_xlabel('{} [{:g}], km/s'.format(fdd_param_keys[j],default_fdds[fdd_param_keys[j]]))
         else:
-            axs[4*j+1].set_xlabel('{} [{:g}]'.format(fdd_param_keys[j],default_fdds[fdd_param_keys[j]]))
-        axs[4*j+1].axvline(default_fdds[fdd_param_keys[j]],linewidth=1.2,color='g') # indicate fdd from vp_setup.dat
-#        axs[4*j+1].margins(y=np.ptp(Dunc_list)/2.0) # Include marging above and below the data to show default fdds
-        axs[4*j+2] = axs[4*j+1].twinx() # second plot on the same axis
-        axs[4*j+2].plot(fdd_list,Hunc_list,color='r')
-        axs[4*j+2].set_ylabel(r'$\Delta$N(H I)',color='r')
-        axs[4*j+2].tick_params('y',colors='r')
-        axs[4*j+2].ticklabel_format(useOffset=False,axis='y')
+            axs[5*j+1].set_xlabel('{} [{:g}]'.format(fdd_param_keys[j],default_fdds[fdd_param_keys[j]]))
+        axs[5*j+1].axvline(default_fdds[fdd_param_keys[j]],linewidth=1,color='g') # indicate fdd from vp_setup.dat
+#        axs[5*j+1].margins(y=np.ptp(Dunc_list)/2.0) # Include marging above and below the data to show default fdds
+        axs[5*j+2] = axs[5*j+1].twinx() # second plot on the same axis
+        axs[5*j+2].plot(fdd_list,Hunc_list,color='r')
+        axs[5*j+2].set_ylabel(r'$\Delta$N(H I)',color='r')
+        axs[5*j+2].tick_params('y',colors='r')
+        axs[5*j+2].ticklabel_format(useOffset=False,axis='y')
         # Second column of subplots (middle values)
-        axs[4*j+3] = plt.subplot(len(fdd_param_keys),2,2*j+2)
+        axs[5*j+3] = plt.subplot(len(fdd_param_keys),3,3*j+2)
         plot_interal = False # Add 1-sigma interval
         if plot_interal == True:
             median  = np.median(D_list)
             med_unc = np.median(Dunc_list)
             print "median = {} +/- {} for {}".format(median,med_unc,fdd_param_keys[j])
-            axs[4*j+3].fill_between(fdd_list, median-med_unc, median+med_unc,facecolor='green', alpha=0.4)
-        axs[4*j+3].plot(fdd_list,D_list) # Plotting data
-        axs[4*j+3].set_ylabel(r'N(D I)',color='b')
-        axs[4*j+3].ticklabel_format(useOffset=False)
-        axs[4*j+3].tick_params('y',colors='b')
-        axs[4*j+3].set_xscale('log')
+            axs[5*j+3].fill_between(fdd_list, median-med_unc, median+med_unc,facecolor='green', alpha=0.4)
+        axs[5*j+3].plot(fdd_list,D_list,color='b') # Plotting data
+        axs[5*j+3].set_ylabel(r'N(D I)',color='b')
+        axs[5*j+3].ticklabel_format(useOffset=False)
+        axs[5*j+3].tick_params('y',colors='b')
+        axs[5*j+3].set_xscale('log')
         if fdd_param_keys[j] == 'fdbstep':
-            axs[4*j+3].set_xlabel('{} [{:g}], km/s'.format(fdd_param_keys[j],default_fdds[fdd_param_keys[j]]))
+            axs[5*j+3].set_xlabel('{} [{:g}], km/s'.format(fdd_param_keys[j],default_fdds[fdd_param_keys[j]]))
         else:
-            axs[4*j+3].set_xlabel('{} [{:g}]'.format(fdd_param_keys[j],default_fdds[fdd_param_keys[j]]))
-        axs[4*j+3].axvline(default_fdds[fdd_param_keys[j]],linewidth=1.2,color='g') # indicate fdd from vp_setup.dat
-#        axs[4*j+3].margins(y=np.ptp(D_list),tight=False) # Include marging above and below the data to show default fdds
-        axs[4*j+4] = axs[4*j+3].twinx()
-        axs[4*j+4].plot(fdd_list,H_list,color='r')
-        axs[4*j+4].set_ylabel(r'N(H I)',color='r')
-        axs[4*j+4].tick_params('y',colors='r')
-        axs[4*j+4].ticklabel_format(useOffset=False,axis='y')
+            axs[5*j+3].set_xlabel('{} [{:g}]'.format(fdd_param_keys[j],default_fdds[fdd_param_keys[j]]))
+        axs[5*j+3].axvline(default_fdds[fdd_param_keys[j]],linewidth=1,color='g') # indicate fdd from vp_setup.dat
+#        axs[5*j+3].margins(y=np.ptp(D_list),tight=False) # Include marging above and below the data to show default fdds
+        axs[5*j+4] = axs[5*j+3].twinx()
+        axs[5*j+4].plot(fdd_list,H_list,color='r')
+        axs[5*j+4].set_ylabel(r'N(H I)',color='r')
+        axs[5*j+4].tick_params('y',colors='r')
+        axs[5*j+4].ticklabel_format(useOffset=False,axis='y')
+        # Third column of subplots (number of iterations)
+        axs[5*j+5] = plt.subplot(len(fdd_param_keys),3,3*j+3)
+        axs[5*j+5].plot(fdd_iter_list,n_iter_list,drawstyle="steps-mid") # Plotting n_iter_list
+#        axs[5*j+5].yaxis.tick_right()
+#        axs[5*j+5].yaxis.set_label_position("right")
+        axs[5*j+5].set_ylabel('N iterations')
+        axs[5*j+5].ticklabel_format(useOffset=False)
+        axs[5*j+5].set_xscale('log')
+        if fdd_param_keys[j] == 'fdbstep':
+            axs[5*j+5].set_xlabel('{} [{:g}], km/s'.format(fdd_param_keys[j],default_fdds[fdd_param_keys[j]]))
+        else:
+            axs[5*j+5].set_xlabel('{} [{:g}]'.format(fdd_param_keys[j],default_fdds[fdd_param_keys[j]]))
+        axs[5*j+5].axvline(default_fdds[fdd_param_keys[j]],linewidth=1,color='g') # indicate fdd from vp_setup.dat
+        axs[5*j+5].axhline(0,linewidth=1,color='grey',ls=":") # indicate fdd from vp_setup.dat
+
 #    plt.tight_layout()
     fig.subplots_adjust(hspace=0.3,wspace=0.9) # Set up spaces between subplots
     plt.savefig("fdd_plot.pdf",bbox_inches='tight', pad_inches=0)
     plt.show()
+    plt.close()
     print "  Plot is done!"
